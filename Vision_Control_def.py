f071b02dd4152591cc344557114c2cac65a9fd4e
RamiroIsaJ <ramfer.isaj@gmail.com>
2022-05-23 10:22:19 -0300

    Control Yeast 0-2-1

---
+++ Vision_Control_def.py
@@ -0,0 +1,161 @@
+import cv2
+import serial
+import numpy as np
+import matplotlib
+from datetime import datetime
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+matplotlib.use("TkAgg")
+
+
+class SaveImages:
+    def __init__(self, window_, id_ima, ini_t):
+        self.window = window_
+        self.id_ima = id_ima
+        self.ini_time = ini_t
+        self.time_c = None
+
+    def diff_time(self, t_end):
+        t_diff = (t_end - self.ini_time).total_seconds()
+        return t_diff
+
+    def save(self, path, name_i, type_, time_c, values, image):
+        self.time_c = time_c
+        filename = path + name_i + str(self.id_ima) + type_
+        now_time = datetime.now()
+        time_sleep = self.diff_time(now_time)
+        if values['_TMI_']:
+            time_sleep /= 60
+        rest_time = np.round(self.time_c - time_sleep, 4)
+        print(rest_time)
+        self.window['_RES_'].update(rest_time)
+        # -----------------------------------------------------------------
+        if rest_time < 0 or self.id_ima == 1:
+            print(filename)
+            print('SAVE IMAGE SUCCESSFULLY')
+            cv2.imwrite(filename, image)
+            self.window['_CIM_'].update(self.id_ima)
+            self.ini_time = datetime.now()
+            self.id_ima += 1
+
+
+class ControlPump:
+    def __init__(self, window_, init_t, ctr, port_n, bauds):
+        self.window = window_
+        self.ini_time = init_t
+        self.control = ctr
+        self.port_name = port_n
+        self.bauds = bauds
+        self.fluid_H, self.fluid_L, self.time_H, self.time_L, self.port = None, None, None, None, None
+        self.area_H, self.area_L = None, None
+
+    def diff_time(self, t_end):
+        t_diff = (t_end - self.ini_time).total_seconds()
+        return t_diff
+
+    def active_pump(self, v_fluid):
+        self.port = serial.Serial(port=self.port_name,
+                                  baudrate=self.bauds,
+                                  bytesize=serial.EIGHTBITS,
+                                  parity=serial.PARITY_NONE,
+                                  stopbits=serial.STOPBITS_ONE)
+        # Start pump
+        self.port.write(b'<<J000R>\n')
+        # Vary flow pump
+        if v_fluid < 100:
+            cad_port = '<<J000F00' + str(v_fluid) + '.0000>\n'
+        else:
+            cad_port = '<<J000F0' + str(v_fluid) + '.0000>\n'
+        self.port.write(bytes(cad_port.encode()))
+        # Stop pump
+        self.port.write(b'<<J000S>\n')
+        self.port.close()
+
+    def control_time(self, fluid_h_, fluid_l_, time_h_, time_l_):
+        self.fluid_H, self.fluid_L, self.time_H, self.time_L = fluid_h_, fluid_l_, time_h_, time_l_
+        now_time = datetime.now()
+        time_sleep = self.diff_time(now_time)
+        time_sleep /= 60
+
+        if self.control:
+            rest_time = np.round(self.time_H - time_sleep, 4)
+            print('Highest fluid : ' + str(self.fluid_H) + ' ul/min.  Remaining time ----->> ' + str(rest_time))
+            if rest_time > 0:
+                self.window['_MES_'].update('Highest fluid is activated')
+                self.active_pump(self.fluid_H)
+            else:
+                self.control = False
+                self.ini_time = datetime.now()
+        else:
+            rest_time = np.round(self.time_L - time_sleep, 4)
+            print('Lowest fluid: ' + str(self.fluid_L) + ' ul/min.  Remaining time ----->> ' + str(rest_time))
+            if rest_time > 0:
+                self.window['_MES_'].update('Lowest fluid is activated')
+                self.active_pump(self.fluid_L)
+            else:
+                self.control = True
+                self.ini_time = datetime.now()
+
+    def control_area(self, fluid_h_, fluid_l_, area_h_, area_l_, m_area):
+        self.fluid_H, self.fluid_L, self.area_H, self.area_L = fluid_h_, fluid_l_, area_h_, area_l_
+
+        if self.control:
+            if m_area < self.area_H:
+                self.active_pump(self.fluid_L)
+                print('----------- Lowest fluid is injected -----------')
+            else:
+                self.control = False
+        else:
+            if m_area > self.area_L:
+                self.active_pump(self.fluid_H)
+                print('----------- Highest fluid is injected -----------')
+            else:
+                self.control = True
+
+
+def bytes_(img_, m, n):
+    ima = cv2.resize(img_, (m, n))
+    return cv2.imencode('.png', ima)[1].tobytes()
+
+
+def camera_idx():
+    index_, videos = 3, []
+    for idx_ in range(index_):
+        cap = cv2.VideoCapture(idx_)
+        if cap.isOpened():
+            cap.release()
+            videos.append(idx_)
+    return videos
+
+
+def update_dir(path):
+    path_s = path.split('/')
+    cad, path_f = len(path_s), path_s[0]
+    for p in range(1, cad):
+        path_f += '\\' + path_s[p]
+    return path_f
+
+
+def serial_test(port_n, bauds):
+    try:
+        c, port = 1, serial.Serial(port=port_n,
+                                   baudrate=bauds,
+                                   bytesize=serial.EIGHTBITS,
+                                   parity=serial.PARITY_NONE,
+                                   stopbits=serial.STOPBITS_ONE)
+        print('--------------------------------------')
+        print('           Port: ' + port.name)
+        print('----------- Test successfully --------')
+
+    except serial.SerialException:
+        print('------- Port is not available ---------')
+        c = 0
+    return c
+
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+
+    return figure_canvas_agg
---
+++ Vision_MT_Control.py
@@ -0,0 +1,354 @@
+# Ramiro Isa-Jara, ramiro.isaj@gmail.com
+# Vision Interface to use for viewing and saving images from Video Camera Input
+# Activate Pump with Analysis of Area of yeast growing ----- version 0.2.1
+# This program uses 2 threads to avoid errors during the execution
+
+import cv2
+import threading
+import time as tm
+import numpy as np
+import pandas as pd
+import PySimpleGUI as sg
+from datetime import datetime
+import Vision_well_def as Vw
+import Vision_Control_def as Vs
+import matplotlib.pyplot as plt
+
+
+def thread_images(path_des_, name_, type_i_, time1_, values_, image_):
+    saveIm.save(path_des_, name_, type_i_, time1_, values_, image_)
+
+
+def thread_pump1(fluid_h_, fluid_l_, time_h_, time_l_):
+    pumpC.control_time(fluid_h_, fluid_l_, time_h_, time_l_)
+
+
+def thread_pump2(fluid_h_, fluid_l_, area_h_, area_l_, area_):
+    pumpC.control_area(fluid_h_, fluid_l_, area_h_, area_l_, area_)
+
+
+# -------------------------------
+# Adjust size screen
+# -------------------------------
+Screen_size = 10
+# -------------------------------
+sg.theme('LightGrey1')
+m1, n1 = 300, 300
+img = np.ones((m1, n1, 1), np.uint8)*255
+
+f1 = np.array([60])
+fig, ax = plt.subplots(figsize=(4, 3), dpi=80)
+ax.plot(f1, 'o-')
+ax.grid()
+
+# --------------------------------------------------------------------------------------------------
+portsWIN = ['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'COM10', 'COM11']
+portsLIN = ['/dev/pts/2', '/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', '/dev/ttyS3']
+
+layout1 = [[sg.Radio('Windows', "RADIO1", enable_events=True, default=True, key='_SYS_')],
+           [sg.Radio('Linux / Mac', "RADIO1", enable_events=True, key='_LIN_')], [sg.Text('')]]
+
+layout2 = [[sg.Checkbox('*.jpg', default=True, key="_IN1_")], [sg.Checkbox('*.png', default=False, key="_IN2_")],
+           [sg.Checkbox('*.tiff', default=False, key="_IN3_")]]
+
+idx = ['0']
+layout3 = [[sg.Radio('Minutes', "RADIO2", enable_events=True, default=True, key='_TMI_'),
+           sg.Radio('Seconds ', "RADIO2", enable_events=True, key='_TSE_')],
+           [sg.Text('Time to wait:', size=(10, 1)), sg.InputText('5', key='_INF_', enable_events=True, size=(7, 1))],
+           [sg.Text('Id_ini image:', size=(10, 1)), sg.InputText('1', key='_IDI_', size=(7, 1))],
+           [sg.Text('Video input: ', size=(10, 1)),
+            sg.Combo(values=idx, size=(6, 1), enable_events=True, key='_VIN_')]]
+
+layout3a = [[sg.T('', size=(5, 1)), sg.Radio('Control - Area', "RADIO3", enable_events=True, default=True,
+             text_color='DarkBlue', key='_CTA_'),
+             sg.T('', size=(7, 1)), sg.Radio('Control - Time', "RADIO3", enable_events=True, key='_CTT_',
+            text_color='DarkBlue'), sg.T('', size=(5, 1))],
+            [sg.Text('Buffer size:', size=(10, 1)), sg.InputText('5', key='_BUF_', size=(7, 1)),
+             sg.Text('img.', size=(5, 1))],
+            [sg.Text('Area MAX:', size=(10, 1)), sg.InputText('70', key='_AMX_', size=(7, 1), enable_events=True),
+             sg.Text('%', size=(5, 1)),
+             sg.Text('Time MAX:', size=(10, 1)), sg.InputText('60', key='_THS_', size=(7, 1), enable_events=True),
+             sg.Text('min', size=(4, 1))],
+            [sg.Text('Area MIN:', size=(10, 1)), sg.InputText('30', key='_AMN_', size=(7, 1), enable_events=True),
+             sg.Text('%', size=(5, 1)),
+             sg.Text('Time MIN:', size=(10, 1)), sg.InputText('5', key='_TLS_', size=(7, 1), enable_events=True),
+             sg.Text('min', size=(4, 1))]]
+
+
+layout4a = [[sg.T('', size=(5, 1))],
+            [sg.Text('* Highest fluid:', size=(12, 1)),
+             sg.InputText('100', key='_HST_', size=(5, 1), enable_events=True), sg.Text('ul/min.', size=(5, 1))],
+            [sg.Text('* Lowest fluid:', size=(12, 1)),
+             sg.InputText('10', key='_LST_', size=(5, 1), enable_events=True), sg.Text('ul/min.', size=(5, 1))]]
+
+layout4b = [[sg.Text('Name: ', size=(9, 1)),
+            sg.Combo(values=portsWIN, size=(9, 1), enable_events=True, key='_PORT_')],
+            [sg.Text('Baudrate:', size=(9, 1)), sg.InputText('9600', key='_RTE_', size=(10, 1))],
+            [sg.Text('Status:', size=(8, 1)), sg.Text('NOT CONNECT', size=(13, 1), key='_CON_', text_color='red')]]
+
+layout5 = [[sg.Text('Name images: ', size=(12, 1)), sg.InputText('Experiment1_', size=(28, 1), key='_NAM_')],
+           [sg.Text('Source path: ', size=(12, 1)), sg.InputText(size=(28, 1), key='_SOU_'), sg.FolderBrowse()],
+           [sg.Text('Destiny path: ', size=(12, 1)), sg.InputText(size=(28, 1), key='_DES_'), sg.FolderBrowse()]]
+
+
+layout7 = [[sg.T("", size=(15, 1)), sg.Text('Current time: ', size=(10, 1)), sg.Text('', size=(10, 1), key='_TAC_')],
+           [sg.T("", size=(2, 1)),
+            sg.Text('Start time: ', size=(8, 1)), sg.Text('-- : -- : --', size=(10, 1), key='_TIN_', text_color='blue'),
+            sg.T("", size=(5, 1)),
+            sg.Text('Finish time: ', size=(8, 1)), sg.Text('-- : -- : --', size=(10, 1), key='_TFI_', text_color='red')],
+           [sg.Text('Waiting time: ', size=(11, 1)), sg.InputText('', key='_RES_', size=(12, 1)),
+            sg.Text('...', size=(1, 1), key='_ITM_'),
+            sg.Text('Total/images: ', size=(12, 1)), sg.InputText('', key='_CIM_', size=(8, 1))],
+           [sg.Text('Name Image: ', size=(11, 1)), sg.InputText('', key='_NIM_', size=(12, 1)),
+            sg.Text(' ', size=(1, 1)),
+            sg.Text('Current Area: ', size=(12, 1)), sg.InputText('', key='_CAR_', size=(8, 1))],
+           [sg.Text('Buffer image: ', size=(11, 1)), sg.InputText('', key='_BUM_', size=(12, 1)),
+            sg.Text(' ', size=(1, 1)),
+            sg.Text('Mean Area:', size=(12, 1)), sg.InputText('', key='_MAR_', size=(8, 1))]
+           ]
+
+v_img = [sg.Image(filename='', key="_IMA_")]
+# columns
+col_1 = [[sg.Frame('', [v_img])], [sg.T("", size=(5, 1))], [sg.Text('Percentage of Area:', size=(18, 1), text_color='DarkRed')], [sg.Canvas(key="_CANVAS_")]]
+col_2 = [[sg.Frame('Operative System: ', layout1, title_color='Blue'),
+          sg.Frame('Type image: ', layout2, title_color='Blue'), sg.Frame('Image settings: ', layout3, title_color='Blue')],
+         [sg.Frame('Save directories: ', layout5, title_color='Blue')],
+         [sg.Frame('Control settings: ', layout3a, title_color='Blue')],
+         [sg.Frame('Pump settings: ', layout4a, title_color='Blue'),
+          sg.Frame('Port settings: ', layout4b, title_color='Blue')],
+         [sg.T(" ", size=(1, 1)), sg.Button('View', size=(8, 1)), sg.Button('Save', size=(8, 1)),
+          sg.Button('Control', size=(8, 1)), sg.Button('Finish', size=(8, 1))],
+         [sg.Frame('', layout7)]]
+
+layout = [[sg.Column(col_1), sg.Column(col_2)]]
+
+# Create the Window
+window = sg.Window('Vision_MT Well-Control', layout, font="Helvetica "+str(Screen_size), finalize=True)
+window['_IMA_'].update(data=Vs.bytes_(img, m1, n1))
+graph = Vs.draw_figure(window["_CANVAS_"].TKCanvas, fig)
+# ----------------------------------------------------------------------------------
+time_, id_image, time_h, time_l, fluid_h, fluid_l, port_name, bauds_, = 0, 1, 0, 0, 0, 0, 0, 0
+area_h, area_l, c_port, id_sys, time_read, area_seq = 0, 0, -1, 0, 0, 0
+view_, save_, pump_, control, ctr_method = False, False, False, True, False
+video, name, image, ini_time, ini_time_, path_ori, path_des, type_i = None, None, None, None, None, None, None, None
+saveIm, pumpC, readIm, cords_well, cont_ini, i, k, buffer, buffer_size, p_area = None, None, None, [], 0, 0, 0, 0, 0, []
+results = pd.DataFrame(columns=['Image', 'Percentage'])
+segYes = Vw.SegmentYeast()
+segYes.build_filters()
+# -----------------------------------------------------------------------------------
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read(timeout=50)
+    window.Refresh()
+    now = datetime.now()
+    now_time = now.strftime("%H : %M : %S")
+    window['_TAC_'].update(now_time)
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == '_VIN_':
+        index = Vs.camera_idx()
+        window.Element('_VIN_').update(values=index)
+
+    if event == '_LIN_':
+        id_sys = 1
+        window.Element('_PORT_').update(values=portsLIN)
+    if event == '_SYS_':
+        id_sys = 0
+        window.Element('_PORT_').update(values=portsWIN)
+
+    if event == '_INF_':
+        if values['_INF_'] != '':
+            time_ = int(values['_INF_']) if int(values['_INF_']) > 0 else 1
+
+    if event == '_HST_':
+        if values['_HST_'] != '':
+            fluid_h = int(values['_HST_']) if int(values['_HST_']) > 10 else 0
+    if event == '_THS_':
+        if values['_THS_'] != '':
+            time_h = float(values['_THS_']) if float(values['_THS_']) > 1 else 1
+    if event == '_LST_':
+        if values['_LST_'] != '':
+            fluid_l = int(values['_LST_']) if int(values['_LST_']) > 1 else 0
+    if event == '_TLS_':
+        if values['_TLS_'] != '':
+            time_l = float(values['_TLS_']) if float(values['_TLS_']) > 1 else 1
+
+    if event == '_PORT_':
+        port_name = values['_PORT_']
+        bauds_ = int(values['_RTE_'])
+        sg.Popup('Serial Port: ', values['_PORT_'])
+        c_port = Vs.serial_test(port_name, bauds_)
+        text = 'CONNECT' if c_port == 1 else 'ERROR'
+        window.Element('_CON_').update(text)
+
+    if event == 'Finish':
+        print('FINISH')
+        window['_IMA_'].update(data=Vs.bytes_(img, m1, n1))
+        if pump_:
+            ax.clear()
+            ax.plot(f1, 'o-')
+            ax.grid()
+            graph.draw()
+            # -------------------------------------------------------------------------------
+            header = values['_NAM_'].split('_')[0]
+            Vw.save_csv_file(results, path_des, header)
+            Vw.graph_data(path_des, header)
+            for i in range(1, 100, 25):
+                sg.OneLineProgressMeter('Saving RESULTS in CSV files', i + 25, 100, 'single')
+                tm.sleep(1)
+            # ---------------------------------------------------------------------------------
+        pump_, ctr_method = False, False
+        cords_well, buffer, p_area, buffer_size, cont_ini, i, k = [], [], [], 0, 0, 0, 0
+        if view_:
+            window.find_element('View').Update(disabled=False)
+            now_time = now.strftime("%H : %M : %S")
+            window['_TFI_'].update(now_time)
+            view_, save_ = False, False
+            video.release()
+
+    if event == 'View':
+        idx = values['_VIN_']
+        if view_ is False and idx != '':
+            video = cv2.VideoCapture(int(idx))
+            now_time1 = now.strftime("%H : %M : %S")
+            window['_TIN_'].update(now_time1)
+            window['_TFI_'].update('-- : -- : --')
+            view_ = True
+            window.find_element('View').update(disabled=True)
+        elif idx == '':
+            sg.Popup('Error', ['Not selected Input Video'])
+        else:
+            sg.Popup('Warning', ['Process is running'])
+
+    if view_:
+        ret, image = video.read()
+        if ret:
+            window['_IMA_'].update(data=Vs.bytes_(image, m1, n1))
+
+    if event == 'Save':
+        print('SAVE PROCESS')
+        id_image = int(values['_IDI_'])
+        if values['_SYS_']:
+            path_ori = Vs.update_dir(values['_SOU_']) + "\\"
+            path_ori = r'{}'.format(path_ori)
+        else:
+            path_ori = values['_SOU_'] + '/'
+        # -------------------------------------------------------------------
+        if values['_TMI_']:
+            window['_ITM_'].update('min')
+        else:
+            window['_ITM_'].update('sec')
+        # -------------------------------------------------------------------
+        if values['_IN2_']:
+            type_i = ".png"
+        elif values['_IN3_']:
+            type_i = ".tiff"
+        else:
+            type_i = ".jpg"
+        # ------------------------------------------------------------------
+        if view_ and len(path_ori) > 1 and save_ is False:
+            ini_time = datetime.now()
+            time_ = float(values['_INF_'])
+            saveIm = Vs.SaveImages(window, id_image, ini_time)
+            name = values['_NAM_']
+            save_ = True
+        elif view_ and len(path_ori) > 1 and save_:
+            sg.Popup('Warning', ['Save images is running...'])
+        else:
+            sg.Popup('Error', ['Information or process is wrong.'])
+
+    if save_:
+        thread = threading.Thread(name="Thread-{}".format(1),
+                                  target=thread_images(path_ori, name, type_i, time_, values, image), args=(saveIm,))
+        thread.setDaemon(True)
+        thread.start()
+
+    if event == 'Control':
+        if values['_SYS_']:
+            path_des = Vs.update_dir(values['_DES_']) + "\\"
+            path_des = r'{}'.format(path_des)
+        else:
+            path_des = values['_DES_'] + '/'
+            path_ori = values['_SOU_'] + '/'
+        type_i = '.jpg'
+        # -------------------------------------
+        fluid_h = int(values['_HST_'])
+        fluid_l = int(values['_LST_'])
+        # -------------------------------------
+        if c_port == 1 and len(path_des) > 1 and pump_ is False:
+            ini_time = datetime.now()
+            time_read = time_ + (time_*0.10)
+            if values['_CTA_']:
+                ctr_method = True
+                readIm = Vw.ReadLastImage(path_ori, type_i, id_image, ini_time, id_sys)
+                area_h = float(values['_AMX_'])
+                area_l = float(values['_AMN_'])
+                buffer_size = int(values['_BUF_'])
+            else:
+                time_h = float(values['_THS_'])
+                time_l = float(values['_TLS_'])
+            # -------------------------------------
+            ini_time_ = datetime.now()
+            pumpC = Vs.ControlPump(window, ini_time_, control, port_name, bauds_)
+            pump_ = True
+        elif c_port == 1 and len(path_des) > 1 and pump_:
+            sg.Popup('Warning', ['Control system is running..'])
+        else:
+            sg.Popup('Error', ['Port not connected or Information is wrong.'])
+
+    if pump_:
+        confirm = readIm.ready_img(time_read, values)
+        if ctr_method:
+            if confirm:
+                image_l, name_l = readIm.load_image()
+                cont_ini, cords_well, ima_res, x, y, radius = segYes.ini_well(image_l, cont_ini, cords_well, buffer_size)
+                k, area_well, mean_area, img_f = segYes.well_main(path_des, ima_res, name_l, type_i, i, k, x, y, radius)
+                area_seq = np.copy(area_well)
+                results = results.append({'Image': name_l, 'Percentage': area_well}, ignore_index=True)
+                window['_IMA_'].update(data=Vs.bytes_(img_f, m1, n1))
+                window['_NIM_'].update(name_l)
+                window['_CAR_'].update(area_well)
+                window['_BUM_'].update(k)
+                # -----------------------------------------------
+                if mean_area > 0:
+                    window['_MAR_'].update(mean_area)
+                # -----------------------------------------------
+                p_area.append(area_well)
+                values_area = np.array(p_area)
+                ax.clear()
+                ax.plot(values_area, 'o-')
+                ax.grid()
+                graph.draw()
+                # -----------------------------------------------
+                thread = threading.Thread(name="Thread-{}".format(2),
+                                          target=thread_pump2(fluid_h, fluid_l, area_h, area_l, area_well),
+                                          args=(pumpC,))
+                thread.setDaemon(True)
+                thread.start()
+            else:
+                thread = threading.Thread(name="Thread-{}".format(2),
+                                          target=thread_pump2(fluid_h, fluid_l, area_h, area_l, area_seq),
+                                          args=(pumpC,))
+                thread.setDaemon(True)
+                thread.start()
+            i += 1
+        else:
+            thread = threading.Thread(name="Thread-{}".format(2),
+                                      target=thread_pump1(fluid_h, fluid_l, time_h, time_l),
+                                      args=(pumpC,))
+            thread.setDaemon(True)
+            thread.start()
+
+    # processing threads and join in main thread to next iteration
+    main_thread = threading.current_thread()
+    for t in threading.enumerate():
+        if t is main_thread:
+            continue
+        t.join()
+
+
+print('CLOSE WINDOW')
+window.close()
